c0b76bf4076c45421e0d3627d75d56543c3f5a8e
He Xian <hexian000@outlook.com>
2024-01-24 14:51:46 +0800

    systemd: make it easy to test error type
    Signed-off-by: He Xian <hexian000@outlook.com>

--- systemd/const.go
+++ systemd/const.go
@@ -1,8 +1,14 @@
 package systemd
 
+import "errors"
+
 const (
 	Ready     = "READY=1"
 	Stopping  = "STOPPING=1"
 	Reloading = "RELOADING=1"
 	Watchdog  = "WATCHDOG=1"
+)
+
+var (
+	ErrUnsupported = errors.New("systemd is not supported on current platform")
 )
--- systemd/notify.go
+++ systemd/notify.go
@@ -2,8 +2,6 @@
 
 package systemd
 
-import "errors"
-
 func Notify(state string) (bool, error) {
-	return false, errors.New("systemd is not supported on current platform")
+	return false, ErrUnsupported
 }
