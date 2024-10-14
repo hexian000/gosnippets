a7a6af4c75024067af561a27b7c1065a1a35ea05
He Xian <hexian000@outlook.com>
2024-10-14 17:30:20 +0800

    slog: check log level early
    Signed-off-by: He Xian <hexian000@outlook.com>

--- slog/logger.go
+++ slog/logger.go
@@ -50,18 +50,15 @@ func (l *Logger) SetOutputConfig(output, tag string) error {
 func (l *Logger) Output(calldepth int, level Level, msg []byte) {
 	now := time.Now()
 	l.mu.RLock()
-	if level > l.level {
-		return
-	}
 	out := l.out
 	filePrefix := l.filePrefix
 	l.mu.RUnlock()
 
 	_, file, line, ok := runtime.Caller(calldepth)
-	if !ok {
-		file, line = "???", 0
-	} else if filePrefix != "" {
+	if ok {
 		file = strings.TrimPrefix(file, filePrefix)
+	} else {
+		file, line = "???", 0
 	}
 
 	out.Write(&message{
@@ -102,65 +99,113 @@ func (l *Logger) SetLineOutput(w io.Writer) {
 }
 
 func (l *Logger) Fatalf(format string, v ...interface{}) {
+	if LevelFatal > l.Level() {
+		return
+	}
 	l.Output(2, LevelFatal, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Fatal(v ...interface{}) {
+	if LevelFatal > l.Level() {
+		return
+	}
 	l.Output(2, LevelFatal, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) Errorf(format string, v ...interface{}) {
+	if LevelError > l.Level() {
+		return
+	}
 	l.Output(2, LevelError, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Error(v ...interface{}) {
+	if LevelError > l.Level() {
+		return
+	}
 	l.Output(2, LevelError, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) Warningf(format string, v ...interface{}) {
+	if LevelWarning > l.Level() {
+		return
+	}
 	l.Output(2, LevelWarning, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Warning(v ...interface{}) {
+	if LevelWarning > l.Level() {
+		return
+	}
 	l.Output(2, LevelWarning, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) Noticef(format string, v ...interface{}) {
+	if LevelNotice > l.Level() {
+		return
+	}
 	l.Output(2, LevelNotice, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Notice(v ...interface{}) {
+	if LevelNotice > l.Level() {
+		return
+	}
 	l.Output(2, LevelNotice, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) Infof(format string, v ...interface{}) {
+	if LevelInfo > l.Level() {
+		return
+	}
 	l.Output(2, LevelInfo, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Info(v ...interface{}) {
+	if LevelInfo > l.Level() {
+		return
+	}
 	l.Output(2, LevelInfo, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) Debugf(format string, v ...interface{}) {
+	if LevelDebug > l.Level() {
+		return
+	}
 	l.Output(2, LevelDebug, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Debug(v ...interface{}) {
+	if LevelDebug > l.Level() {
+		return
+	}
 	l.Output(2, LevelDebug, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) Verbosef(format string, v ...interface{}) {
+	if LevelVerbose > l.Level() {
+		return
+	}
 	l.Output(2, LevelVerbose, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) Verbose(v ...interface{}) {
+	if LevelVerbose > l.Level() {
+		return
+	}
 	l.Output(2, LevelVerbose, []byte(fmt.Sprint(v...)))
 }
 
 func (l *Logger) VeryVerbosef(format string, v ...interface{}) {
+	if LevelVeryVerbose > l.Level() {
+		return
+	}
 	l.Output(2, LevelVeryVerbose, []byte(fmt.Sprintf(format, v...)))
 }
 
 func (l *Logger) VeryVerbose(v ...interface{}) {
+	if LevelVeryVerbose > l.Level() {
+		return
+	}
 	l.Output(2, LevelVeryVerbose, []byte(fmt.Sprint(v...)))
 }
--- slog/slog.go
+++ slog/slog.go
@@ -38,80 +38,128 @@ func CheckLevel(level Level) bool {
 
 // Serious problems that are likely to cause the program to exit.
 func Fatalf(format string, v ...interface{}) {
+	if !CheckLevel(LevelFatal) {
+		return
+	}
 	std.Output(2, LevelFatal, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Serious problems that are likely to cause the program to exit.
 func Fatal(v ...interface{}) {
+	if !CheckLevel(LevelFatal) {
+		return
+	}
 	std.Output(2, LevelFatal, []byte(fmt.Sprint(v...)))
 }
 
 // Issues that shouldn't be ignored.
 func Errorf(format string, v ...interface{}) {
+	if !CheckLevel(LevelError) {
+		return
+	}
 	std.Output(2, LevelError, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Issues that shouldn't be ignored.
 func Error(v ...interface{}) {
+	if !CheckLevel(LevelError) {
+		return
+	}
 	std.Output(2, LevelError, []byte(fmt.Sprint(v...)))
 }
 
 // Issues that may be ignored.
 func Warningf(format string, v ...interface{}) {
+	if !CheckLevel(LevelWarning) {
+		return
+	}
 	std.Output(2, LevelWarning, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Issues that may be ignored.
 func Warning(v ...interface{}) {
+	if !CheckLevel(LevelWarning) {
+		return
+	}
 	std.Output(2, LevelWarning, []byte(fmt.Sprint(v...)))
 }
 
 // Important status changes. The prefix is 'I'.
 func Noticef(format string, v ...interface{}) {
+	if !CheckLevel(LevelNotice) {
+		return
+	}
 	std.Output(2, LevelNotice, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Important status changes. The prefix is 'I'.
 func Notice(v ...interface{}) {
+	if !CheckLevel(LevelNotice) {
+		return
+	}
 	std.Output(2, LevelNotice, []byte(fmt.Sprint(v...)))
 }
 
 // Normal work reports.
 func Infof(format string, v ...interface{}) {
+	if !CheckLevel(LevelInfo) {
+		return
+	}
 	std.Output(2, LevelInfo, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Normal work reports.
 func Info(v ...interface{}) {
+	if !CheckLevel(LevelInfo) {
+		return
+	}
 	std.Output(2, LevelInfo, []byte(fmt.Sprint(v...)))
 }
 
 // Extra information for debugging.
 func Debugf(format string, v ...interface{}) {
+	if !CheckLevel(LevelDebug) {
+		return
+	}
 	std.Output(2, LevelDebug, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Extra information for debugging.
 func Debug(v ...interface{}) {
+	if !CheckLevel(LevelDebug) {
+		return
+	}
 	std.Output(2, LevelDebug, []byte(fmt.Sprint(v...)))
 }
 
 // Details for inspecting specific issues.
 func Verbosef(format string, v ...interface{}) {
+	if !CheckLevel(LevelVerbose) {
+		return
+	}
 	std.Output(2, LevelVerbose, []byte(fmt.Sprintf(format, v...)))
 }
 
 // Details for inspecting specific issues.
 func Verbose(v ...interface{}) {
+	if !CheckLevel(LevelVerbose) {
+		return
+	}
 	std.Output(2, LevelVerbose, []byte(fmt.Sprint(v...)))
 }
 
 // More details that may significantly impact performance. The prefix is 'V'.
 func VeryVerbosef(format string, v ...interface{}) {
+	if !CheckLevel(LevelVeryVerbose) {
+		return
+	}
 	std.Output(2, LevelVeryVerbose, []byte(fmt.Sprintf(format, v...)))
 }
 
 // More details that may significantly impact performance. The prefix is 'V'.
 func VeryVerbose(v ...interface{}) {
+	if !CheckLevel(LevelVeryVerbose) {
+		return
+	}
 	std.Output(2, LevelVeryVerbose, []byte(fmt.Sprint(v...)))
 }
